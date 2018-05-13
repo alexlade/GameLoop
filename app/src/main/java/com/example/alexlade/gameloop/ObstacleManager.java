07e7c6b18be6f2768a3d5bb3c3b291e817f3e21d
alexlade <36495528+alexlade@users.noreply.github.com>
2018-05-13 12:40:48 -0500

    collision detection and score

--- app/src/main/java/com/example/alexlade/gameloop/ObstacleManager.java
+++ app/src/main/java/com/example/alexlade/gameloop/ObstacleManager.java
@@ -2,6 +2,7 @@ package com.example.alexlade.gameloop;
 
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.Paint;
 
 import java.util.ArrayList;
 
@@ -15,6 +16,9 @@ public class ObstacleManager {
     private int color;
 
     private long startTime;
+    private long initTime;
+
+    private int score = 0;
 
     public ObstacleManager(int playerGap,int obstacleGap,int obstacleHeight,int color){
         this.playerGap = playerGap;
@@ -22,12 +26,20 @@ public class ObstacleManager {
         this.obstacleHeight = obstacleHeight;
         this.color = color;
 
-        startTime = System.currentTimeMillis();
+        startTime = initTime = System.currentTimeMillis();
 
         obstacles = new ArrayList<>();
 
         populateObstacles();
 
+    }
+
+    public boolean playerCollide(RectPlayer player){
+        for(Obstacle ob : obstacles){
+            if(ob.playerCollide((player)))
+                return true;
+        }
+        return false;
     }
 
     private void populateObstacles(){
@@ -44,20 +56,25 @@ public class ObstacleManager {
     public void update(){
         int elapsedTime = (int)(System.currentTimeMillis() - startTime);
         startTime = System.currentTimeMillis();
-        float speed = Constants.SCREEN_HEIGHT/10000.0f;
+        float speed = (float)(Math.sqrt(1+(startTime - initTime)/2000.0))* Constants.SCREEN_HEIGHT/(10000.0f);
         for(Obstacle ob: obstacles){
-            ob.incrementY(speed + elapsedTime);
+            ob.incrementY(speed * elapsedTime);
         }
         if(obstacles.get(obstacles.size()-1).getRectangle().top >= Constants.SCREEN_HEIGHT){
             int xStart = (int) (Math.random() * (Constants.SCREEN_WIDTH - playerGap));
             obstacles.add(0,new Obstacle(obstacleHeight,color,xStart,obstacles.get(0).getRectangle().top-obstacleHeight-obstacleGap,playerGap));
             obstacles.remove(obstacles.size()-1);
+            score++;
         }
     }
 
     public void draw(Canvas canvas){
         for(Obstacle ob : obstacles)
             ob.draw(canvas);
+        Paint paint = new Paint();
+        paint.setTextSize(100);
+        paint.setColor(Color.MAGENTA);
+        canvas.drawText(""+score,50,50+paint.descent()-paint.ascent(),paint);
 
     }
 }
